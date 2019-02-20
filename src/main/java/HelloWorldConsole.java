0dca92c282bde80abea8bbcfc83941fe05578c17
unknown <16erikami@gmail.com>
2019-02-20 19:56:50 +0100

    Initial commit

---
+++ src/main/java/HelloWorldConsole.java
@@ -0,0 +1,10 @@
+public class HelloWorldConsole {
+
+    /**
+     * Main metoda
+     * @param args vstupne argumenty
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world !");
+    }
+}
